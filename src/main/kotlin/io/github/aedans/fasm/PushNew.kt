c55a4ddd89da60714a64c783fe45911e18685ce5
Aedan Smith <aedan.smith@gmail.com>
2018-03-01 15:39:55 -0800

    Add args to PushNew

--- src/main/kotlin/io/github/aedans/fasm/PushNew.kt
+++ src/main/kotlin/io/github/aedans/fasm/PushNew.kt
@@ -2,10 +2,11 @@ package io.github.aedans.fasm
 
 import org.objectweb.asm.commons.GeneratorAdapter
 
-data class PushNew(val type: Type, val methodType: MethodType) : Operation {
+data class PushNew(val type: Type, val methodType: MethodType, val args: List<Operation>) : Operation {
     override fun generate(generatorAdapter: GeneratorAdapter) {
         New(type).generate(generatorAdapter)
         Dup.generate(generatorAdapter)
+        args.forEach { it.generate(generatorAdapter) }
         InvokeConstructor(type, methodType).generate(generatorAdapter)
     }
 }
